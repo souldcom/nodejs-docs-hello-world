893cf610b8eb1df0945470938ce97da87d9aedf5
souldcom <110030537+souldcom@users.noreply.github.com>
2022-07-29 09:45:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((request, response) => {
     response.end("Hello World my Friend");
 });
 
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 6060;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
